7a29f583abcae37325ee318dc73a85fef067b10f
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-01-30 00:07:13 -0600

    cleaned up models folder

--- server/models/Post.js
+++ server/models/Post.js
@@ -18,26 +18,7 @@ const postSchema = new Schema({
     type: Date,
     default: Date.now,
     get: (timestamp) => dateFormat(timestamp),
-  },
-  // comments: [
-  //   {
-  //     commentText: {
-  //       type: String,
-  //       required: true,
-  //       minlength: 1,
-  //       maxlength: 280,
-  //     },
-  //     commentAuthor: {
-  //       type: String,
-  //       required: true,
-  //     },
-  //     createdAt: {
-  //       type: Date,
-  //       default: Date.now,
-  //       get: (timestamp) => dateFormat(timestamp),
-  //     },
-  //   },
-  // ],
+  }
 });
 
 const Post = model('Post', postSchema);
@@ -46,30 +27,3 @@ module.exports = Post;
 
 
 
-// const mongoose = require("mongoose");
-// mongoose.pluralize(null);
-
-// const collectionName = "user_post"; //process.env.COL_USER_POST;
-// const Schema = mongoose.Schema;
-
-// const userPostSchema = new Schema({
-//   name: String,
-//   posts: [
-//     {
-//       status: String,
-//       createdDate: Date,
-//       updatedDate: Date,
-//       deletedDate: Date,
-//       location: { late: Number, long: Number },
-//     },
-//   ],
-// });
-
-// userPostSchema.pre("save", function (next) {
-//   var currentDate = new Date();
-//   this.updatedDate = currentDate;
-//   if (!this.createdDate) this.createdDate = currentDate;
-//   next();
-// });
-
-// module.exports = mongoose.model(collectionName, userPostSchema);
